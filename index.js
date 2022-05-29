cb7f316be44f6ea002c5555e40e32379b94538fa
Rahim Mahat <rahimmahat99@gmail.com>
2022-05-29 20:17:48 +0530

    about me section

--- index.js
+++ index.js
@@ -41,18 +41,37 @@ document.body.addEventListener("mousemove", (e) => {
 })
 
 // Main Button
-const mainBtn = document.querySelector(".main-btn")
-let ripple;
-mainBtn.addEventListener("mouseenter", (e) => {
-    const left = e.clientX - e.target.getBoundingClientRect().left
-    const top = e.clientY - e.target.getBoundingClientRect().top
-    ripple = document.createElement("div");
-    ripple.classList.add("ripple")
-    ripple.style.left = `${left}px`
-    ripple.style.top = `${top}px`
-    mainBtn.prepend(ripple);
+const mainBtns = document.querySelectorAll(".main-btn")
+
+mainBtns.forEach((btn) => {
+    let ripple;
+    btn.addEventListener("mouseenter", (e) => {
+        const left = e.clientX - e.target.getBoundingClientRect().left
+        const top = e.clientY - e.target.getBoundingClientRect().top
+        ripple = document.createElement("div");
+        ripple.classList.add("ripple")
+        ripple.style.left = `${left}px`
+        ripple.style.top = `${top}px`
+        btn.prepend(ripple);
+    })
+
+    btn.addEventListener("mouseleave", () => {
+        btn.removeChild(ripple)
+    })
 })
 
-mainBtn.addEventListener("mouseleave", () => {
-    mainBtn.removeChild(ripple)
+
+// About me text
+
+const AboutMeText = document.querySelector(".about-me-text")
+const AboutMeTextContent = `I am a Developer & a Designer i create best websites not just development but also the designing keeping the user experience in mind, also an CyberSecurity enthusiast so can also do the basic pentesting/bug-hunting`
+
+Array.from(AboutMeTextContent).forEach((char) => {
+    const span = document.createElement("span")
+    span.textContent = char
+    AboutMeText.appendChild(span)
+
+    span.addEventListener("mouseenter", (e) => {
+        e.target.style.animation = "aboutMeTextAnim 7s infinite"
+    })
 })
