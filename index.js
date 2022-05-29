614eeef5fc067e07253b13ca276686c20a957c52
Rahim Mahat <rahimmahat99@gmail.com>
2022-05-29 13:07:08 +0530

    landing page

---
+++ index.js
@@ -0,0 +1,58 @@
+// Animated Circles
+const circles = document.querySelectorAll(".circle")
+const mainImg = document.querySelector(".main-circle img")
+
+let mX = 0
+let mY = 0
+const z = 100;
+const AnimateCircles = (e, x, y) => {
+
+    if (x < mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `${z}px`;
+        })
+        mainImg.style.left = `${z}px`
+    } else if (x > mX) {
+        circles.forEach((circle) => {
+            circle.style.left = `-${z}px`;
+        })
+        mainImg.style.left = `-${z}px`
+    }
+    if (y < mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `${z}px`;
+        })
+        mainImg.style.top = `${z}px`
+    } else if (y > mY) {
+        circles.forEach((circle) => {
+            circle.style.top = `-${z}px`;
+        })
+        mainImg.style.top = `-${z}px`
+    }
+    mX = e.clientX;
+    mY = e.clientY;
+}
+
+document.body.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    AnimateCircles(e, x, y)
+})
+
+// Main Button
+const mainBtn = document.querySelector(".main-btn")
+let ripple;
+mainBtn.addEventListener("mouseenter", (e) => {
+    const left = e.clientX - e.target.getBoundingClientRect().left
+    const top = e.clientY - e.target.getBoundingClientRect().top
+    ripple = document.createElement("div");
+    ripple.classList.add("ripple")
+    ripple.style.left = `${left}px`
+    ripple.style.top = `${top}px`
+    mainBtn.prepend(ripple);
+})
+
+mainBtn.addEventListener("mouseleave", () => {
+    mainBtn.removeChild(ripple)
+})
