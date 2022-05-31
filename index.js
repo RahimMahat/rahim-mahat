3d445930de5e17c22ffc6022769dc9ab862859a5
Rahim Mahat <rahimmahat99@gmail.com>
2022-05-31 12:51:34 +0530

    form

--- index.js
+++ index.js
@@ -270,11 +270,15 @@ const checkEmail = (input) => {
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+    checkLength(username, 2);
+    checkLength(subject, 2);
+    checkLength(message, 10);
+    checkEmail(email);
+    checkRequiredFields([username, email, subject, message]);
 
-    checkLength(username, 2)
-    checkLength(subject, 2)
-    checkLength(message, 10)
-    checkEmail(email)
-    checkRequiredFiels([username, email, subject, message])
-})
+    const notValid = Array.from(messages).find((message) => {
+        return message.classList.contains("error");
+    });
+
+    notValid && e.preventDefault();
+});
